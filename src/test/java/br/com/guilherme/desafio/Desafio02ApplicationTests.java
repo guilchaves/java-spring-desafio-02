cea57337852af60f961370712bbb9cc0fc72338f
guilherme <chaves.guilherme@proton.me>
2023-12-04 21:25:58 -0300

    initial commit

---
+++ src/test/java/br/com/guilherme/desafio/Desafio02ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guilherme.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
