050150e4b9a6345136121bffc21fd92bc993eb1c
guilherme <chaves.guilherme@proton.me>
2023-12-08 19:19:56 -0300

    feat: add GET participantes by id and POST participantes

--- src/main/java/br/com/guilherme/desafio/controllers/ParticipanteController.java
+++ src/main/java/br/com/guilherme/desafio/controllers/ParticipanteController.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/participantes")
@@ -18,9 +19,27 @@ public class ParticipanteController {
     private ParticipanteService service;
 
     @GetMapping
-    public ResponseEntity<Page<ParticipanteDTO>> findAll(Pageable pageable){
+    public ResponseEntity<Page<ParticipanteDTO>> findAll(Pageable pageable) {
         Page<ParticipanteDTO> dto = service.findAll(pageable);
         return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ParticipanteDTO> findById(@PathVariable Integer id) {
+        ParticipanteDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ParticipanteDTO> insert(@RequestBody ParticipanteDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(dto);
     }
 }
 
--- src/main/java/br/com/guilherme/desafio/services/ParticipanteService.java
+++ src/main/java/br/com/guilherme/desafio/services/ParticipanteService.java
@@ -3,11 +3,13 @@ package br.com.guilherme.desafio.services;
 import br.com.guilherme.desafio.dto.ParticipanteDTO;
 import br.com.guilherme.desafio.entities.Participante;
 import br.com.guilherme.desafio.repositories.ParticipanteRepository;
+import br.com.guilherme.desafio.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class ParticipanteService {
@@ -19,5 +21,26 @@ public class ParticipanteService {
     public Page<ParticipanteDTO> findAll(Pageable pageable) {
         Page<Participante> result = repository.findAll(pageable);
         return result.map(ParticipanteDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public ParticipanteDTO findById(Integer id) {
+        Participante entity = repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Resource not found")
+        );
+        return new ParticipanteDTO(entity);
+    }
+
+    @Transactional
+    public ParticipanteDTO insert(@RequestBody ParticipanteDTO dto) {
+        Participante entity = new Participante();
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ParticipanteDTO(entity);
+    }
+
+    private void copyDtoToEntity(ParticipanteDTO dto, Participante entity) {
+        entity.setNome(dto.getNome());
+        entity.setEmail(dto.getEmail());
     }
 }
